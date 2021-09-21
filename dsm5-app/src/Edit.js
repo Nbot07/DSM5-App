acfa6431b3881009ff755496816c094c4609c0f5
Nbot07 <nseobot@buffalo.edu>
2021-09-21 03:53:09 -0400

    choose drag and drop chart for edit page

--- dsm5-app/src/Edit.js
+++ dsm5-app/src/Edit.js
@@ -1,12 +1,14 @@
 import { useParams } from "react-router";
-import EditChart from "./edit-chart/edit-chart";
+import DragDropChart from "./drag-drop-chart/drag-drop-chart";
+
 
 const Edit = () => {
     const { name } = useParams();
     return ( 
         <>
         <h2>Edit Page for { name } Tree</h2>
-            <EditChart/>
+        <h3>Click and drag to rearrange Tree</h3>
+            <DragDropChart/>
         </>
      );
 }
